bd615ad2e529600c9daaa37d3a3719e13b3ac054
Hanno Braun <hb@hannobraun.de>
2020-11-14 18:32:07 +0100

    Print position only once per second

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,13 @@ struct GamePlugin;
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.add_startup_system(setup.system())
+        app.add_resource(PrintTimer(Timer::from_seconds(1.0, true)))
+            .add_startup_system(setup.system())
             .add_system(print_position.system());
     }
 }
+
+struct PrintTimer(Timer);
 
 #[derive(Debug)]
 struct Position(Vec2);
@@ -24,6 +27,14 @@ fn setup(mut commands: Commands) {
     commands.spawn((Position(Vec2::new(0.0, 0.0)),));
 }
 
-fn print_position(position: &Position) {
-    println!("{:?}", position);
+fn print_position(
+    time: Res<Time>,
+    mut timer: ResMut<PrintTimer>,
+    position: &Position,
+) {
+    timer.0.tick(time.delta_seconds);
+
+    if timer.0.finished {
+        println!("{:?}", position);
+    }
 }
