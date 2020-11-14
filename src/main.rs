7462613b35ffed4e326cbd7844c3244504ac1b50
Hanno Braun <hb@hannobraun.de>
2020-11-14 16:29:57 +0100

    Add position and print it

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::build().add_system(hello_world.system()).run();
+    App::build()
+        .add_startup_system(setup.system())
+        .add_system(hello_world.system())
+        .add_system(print_position.system())
+        .run();
 }
 
 fn hello_world() {
     println!("Hello, world!");
+}
+
+#[derive(Debug)]
+struct Position(Vec2);
+
+fn setup(mut commands: Commands) {
+    commands.spawn((Position(Vec2::new(0.0, 0.0)),));
+}
+
+fn print_position(position: &Position) {
+    println!("{:?}", position);
 }
