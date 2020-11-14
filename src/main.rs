66da3f613005823a4482513a272b0b9e371bb23f
Hanno Braun <hb@hannobraun.de>
2020-11-14 18:00:44 +0100

    Bundle game logic into plugin

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,18 @@ use bevy::{input, prelude::*};
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
+        .add_plugin(GamePlugin)
         .add_system(input::system::exit_on_esc_system.system())
-        .add_startup_system(setup.system())
-        .add_system(print_position.system())
         .run();
+}
+
+struct GamePlugin;
+
+impl Plugin for GamePlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_startup_system(setup.system())
+            .add_system(print_position.system());
+    }
 }
 
 #[derive(Debug)]
