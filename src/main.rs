fedd37ac01d9e8f581e2c217a18df176a9918b34
Hanno Braun <hb@hannobraun.de>
2020-12-06 10:55:46 +0100

    Update nav marker size according to ship thrust

--- src/main.rs
+++ src/main.rs
@@ -202,15 +202,14 @@ fn update_heading(
     }
 }
 
-// TASK: Update nav marker's size, according to ship thrust.
 fn update_nav_marker(
     bodies: Res<RigidBodySet>,
-    players: Query<(&Player, &RigidBodyHandleComponent)>,
-    mut nav_markers: Query<&mut Transform>,
+    players: Query<(&Player, &Ship, &RigidBodyHandleComponent)>,
+    mut nav_markers: Query<(&mut Transform, &mut Sprite)>,
 ) {
-    for (player, body) in players.iter() {
+    for (player, ship, body) in players.iter() {
         let body = bodies.get(body.handle()).unwrap();
-        let mut transform =
+        let (mut transform, mut sprite) =
             nav_markers.get_mut(player.nav_marker.entity).unwrap();
 
         let dir = player.nav_marker.direction.normalize();
@@ -220,5 +219,10 @@ fn update_nav_marker(
         *transform = Transform::from_translation(Vec3::new(
             position.x, position.y, LAYER_UI,
         ));
+
+        let min_size = 5.0;
+        let max_size = 25.0;
+        let size = min_size + (max_size - min_size) * ship.thrust;
+        *sprite = Sprite::new(Vec2::new(size, size));
     }
 }
