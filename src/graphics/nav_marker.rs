2b93fc52cccae9b95222e21602c6d2d2df76c5f6
Hanno Braun <hb@hannobraun.de>
2020-12-08 18:44:19 +0100

    Make systems more robust

--- src/graphics/nav_marker.rs
+++ src/graphics/nav_marker.rs
@@ -21,16 +21,18 @@ fn update_position(
 ) {
     for (player, body) in players.iter() {
         let body = bodies.get(body.handle()).unwrap();
-        let (nav_marker, mut transform) =
-            nav_markers.get_mut(player.nav_marker).unwrap();
 
-        let dir = nav_marker.direction.normalize();
+        if let Ok((nav_marker, mut transform)) =
+            nav_markers.get_mut(player.nav_marker)
+        {
+            let dir = nav_marker.direction.normalize();
 
-        let position = body.position().translation.vector
-            + na::Vector2::new(dir.x(), dir.y()) * 250.0;
-        *transform = Transform::from_translation(Vec3::new(
-            position.x, position.y, LAYER_UI,
-        ));
+            let position = body.position().translation.vector
+                + na::Vector2::new(dir.x(), dir.y()) * 250.0;
+            *transform = Transform::from_translation(Vec3::new(
+                position.x, position.y, LAYER_UI,
+            ));
+        }
     }
 }
 
@@ -39,11 +41,11 @@ fn update_size(
     mut nav_markers: Query<&mut Sprite>,
 ) {
     for (player, ship) in players.iter() {
-        let mut sprite = nav_markers.get_mut(player.nav_marker).unwrap();
-
-        let min_size = 5.0;
-        let max_size = 25.0;
-        let size = min_size + (max_size - min_size) * ship.thrust;
-        *sprite = Sprite::new(Vec2::new(size, size));
+        if let Ok(mut sprite) = nav_markers.get_mut(player.nav_marker) {
+            let min_size = 5.0;
+            let max_size = 25.0;
+            let size = min_size + (max_size - min_size) * ship.thrust;
+            *sprite = Sprite::new(Vec2::new(size, size));
+        }
     }
 }
