d3335083a433e73fc54422c7740823723d6b44db
Hanno Braun <hb@hannobraun.de>
2020-12-14 07:34:05 +0100

    Move world setup to `world`

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ mod input;
 mod world;
 
 use bevy::{input::system::exit_on_esc_system, prelude::*};
-use bevy_rapier2d::{na, physics::RapierConfiguration};
 use slog::{info, o, Drain as _, Logger};
 use world::player::Player;
 
@@ -20,26 +19,10 @@ fn main() {
     App::build()
         .add_resource(log)
         .add_plugins(DefaultPlugins)
-        .add_plugin(WorldPlugin)
+        .add_plugin(world::WorldPlugin)
         .add_plugin(camera::CameraPlugin)
         .add_plugin(crate::input::InputPlugin)
         .add_plugin(crate::graphics::GraphicsPlugin)
         .add_system(exit_on_esc_system.system())
         .run();
-}
-
-// TASK: Integrate into `world::WorldPlugin`.
-struct WorldPlugin;
-
-impl Plugin for WorldPlugin {
-    fn build(&self, app: &mut AppBuilder) {
-        app.add_plugin(world::WorldPlugin)
-            .add_startup_system(setup.system());
-    }
-}
-
-fn setup(mut rapier: ResMut<RapierConfiguration>, log: Res<Logger>) {
-    rapier.gravity = na::Vector2::zeros();
-
-    info!(log, "Set up world.");
 }
--- src/world/mod.rs
+++ src/world/mod.rs
@@ -3,15 +3,26 @@ pub mod rock;
 pub mod ship;
 
 use bevy::prelude::*;
-use bevy_rapier2d::physics::RapierPhysicsPlugin;
+use bevy_rapier2d::{
+    na,
+    physics::{RapierConfiguration, RapierPhysicsPlugin},
+};
+use slog::{info, Logger};
 
 pub struct WorldPlugin;
 
 impl Plugin for WorldPlugin {
     fn build(&self, app: &mut AppBuilder) {
         app.add_plugin(RapierPhysicsPlugin)
+            .add_startup_system(setup.system())
             .add_plugin(player::PlayerPlugin)
             .add_plugin(rock::RockPlugin)
             .add_plugin(ship::ShipPlugin);
     }
+}
+
+fn setup(mut rapier: ResMut<RapierConfiguration>, log: Res<Logger>) {
+    rapier.gravity = na::Vector2::zeros();
+
+    info!(log, "Set up world.");
 }
