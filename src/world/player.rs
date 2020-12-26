b2ca3ca3b746518434964c14f874fba751c5108e
Hanno Braun <hb@hannobraun.de>
2020-12-26 09:20:07 +0100

    Convert free function into associated function

--- src/world/player.rs
+++ src/world/player.rs
@@ -4,16 +4,18 @@ pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.add_startup_system(setup.system());
+        app.add_startup_system(Self::setup.system());
+    }
+}
+
+impl PlayerPlugin {
+    fn setup(commands: &mut Commands) {
+        commands.spawn((Player {
+            direction_setting: Vec2::unit_x(),
+        },));
     }
 }
 
 pub struct Player {
     pub direction_setting: Vec2,
-}
-
-fn setup(commands: &mut Commands) {
-    commands.spawn((Player {
-        direction_setting: Vec2::unit_x(),
-    },));
 }
