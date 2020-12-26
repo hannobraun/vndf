21564647399bdb32c32a6c4bc24410e5056499ac
Hanno Braun <hb@hannobraun.de>
2020-12-26 09:33:15 +0100

    Convert free function into associated function

--- src/world/rock.rs
+++ src/world/rock.rs
@@ -8,7 +8,30 @@ pub struct RockPlugin;
 
 impl Plugin for RockPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.add_startup_system(setup.system());
+        app.add_startup_system(Self::setup.system());
+    }
+}
+
+impl RockPlugin {
+    // TASK: Make rocks round. At this point, I only know how to easily display
+    //       rectangular sprites, but once we get accessible 2D drawing
+    //       primitives, it would be nice to make rocks round.
+    // TASK: Convert into regular system that is called every frame.
+    fn setup(commands: &mut Commands) {
+        RockSpawner.spawn_rocks(
+            Rect {
+                left: -2500.0,
+                right: 2500.0,
+                top: -2500.0,
+                bottom: 2500.0,
+            },
+            |x, y, size| {
+                commands
+                    .spawn((Rock { size },))
+                    .with(RigidBodyBuilder::new_dynamic().translation(x, y))
+                    .with(ColliderBuilder::cuboid(size / 2.0, size / 2.0));
+            },
+        )
     }
 }
 
@@ -20,27 +43,6 @@ impl Rock {
     pub fn size(&self) -> f32 {
         self.size
     }
-}
-
-// TASK: Make rocks round. At this point, I only know how to easily display
-//       rectangular sprites, but once we get accessible 2D drawing primitives,
-//       it would be nice to make rocks round.
-// TASK: Convert into regular system that is called every frame.
-fn setup(commands: &mut Commands) {
-    RockSpawner.spawn_rocks(
-        Rect {
-            left: -2500.0,
-            right: 2500.0,
-            top: -2500.0,
-            bottom: 2500.0,
-        },
-        |x, y, size| {
-            commands
-                .spawn((Rock { size },))
-                .with(RigidBodyBuilder::new_dynamic().translation(x, y))
-                .with(ColliderBuilder::cuboid(size / 2.0, size / 2.0));
-        },
-    )
 }
 
 struct RockSpawner;
