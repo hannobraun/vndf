dde200f8ffe0e32df7f17bb1da065083d3962551
Hanno Braun <hb@hannobraun.de>
2020-12-27 10:18:45 +0100

    Refactor

--- src/world/ship.rs
+++ src/world/ship.rs
@@ -1,4 +1,4 @@
-use bevy_rapier2d::{na, rapier::dynamics::RigidBody};
+use bevy_rapier2d::{na::Vector2, rapier::dynamics::RigidBody};
 
 pub const SHIP_SIZE: [f32; 2] = [150.0, 50.0];
 
@@ -19,7 +19,7 @@ impl Ship {
     }
 
     pub fn apply_thrust(&self, body: &mut RigidBody) {
-        let direction = body.position().rotation * na::Vector2::new(1.0, 0.0);
+        let direction = body.position().rotation * Vector2::new(1.0, 0.0);
 
         let thrust = 1_000_000.0 * direction;
         body.apply_force(self.thrust_setting * thrust, true);
