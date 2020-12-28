7cb947b987c6ac3fc49c33f80678651976c64eb5
Hanno Braun <hb@hannobraun.de>
2020-12-28 10:57:25 +0100

    Convert free functions into associated functions

--- src/ui.rs
+++ src/ui.rs
@@ -7,52 +7,54 @@ pub struct UiPlugin;
 
 impl Plugin for UiPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.add_startup_system(setup.system())
-            .add_system(update_frame_time.system());
+        app.add_startup_system(Self::setup.system())
+            .add_system(Self::update_frame_time.system());
     }
 }
 
-struct FrameTime;
-
-fn setup(commands: &mut Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(CameraUiBundle::default());
-    commands
-        .spawn(TextBundle {
-            text: Text {
-                font: asset_server.load("fonts/Tuffy_Bold.ttf"),
-                style: TextStyle {
-                    font_size: 32.0,
-                    color: Color::WHITE,
+impl UiPlugin {
+    fn setup(commands: &mut Commands, asset_server: Res<AssetServer>) {
+        commands.spawn(CameraUiBundle::default());
+        commands
+            .spawn(TextBundle {
+                text: Text {
+                    font: asset_server.load("fonts/Tuffy_Bold.ttf"),
+                    style: TextStyle {
+                        font_size: 32.0,
+                        color: Color::WHITE,
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                },
+                style: Style {
+                    position_type: PositionType::Absolute,
+                    position: Rect {
+                        top: Val::Px(10.0),
+                        left: Val::Px(10.0),
+                        ..Default::default()
+                    },
                     ..Default::default()
                 },
                 ..Default::default()
-            },
-            style: Style {
-                position_type: PositionType::Absolute,
-                position: Rect {
-                    top: Val::Px(10.0),
-                    left: Val::Px(10.0),
-                    ..Default::default()
-                },
-                ..Default::default()
-            },
-            ..Default::default()
-        })
-        .with(FrameTime);
-}
+            })
+            .with(FrameTime);
+    }
 
-fn update_frame_time(
-    diagnostics: Res<Diagnostics>,
-    mut elements: Query<&mut Text, With<FrameTime>>,
-) {
-    for mut text in elements.iter_mut() {
-        if let Some(frame_time) =
-            diagnostics.get(FrameTimeDiagnosticsPlugin::FRAME_TIME)
-        {
-            if let Some(frame_time) = frame_time.average() {
-                let frame_time_ms = frame_time * 1000.0;
-                text.value = format!("Frame Time: {:.0} ms", frame_time_ms);
+    fn update_frame_time(
+        diagnostics: Res<Diagnostics>,
+        mut elements: Query<&mut Text, With<FrameTime>>,
+    ) {
+        for mut text in elements.iter_mut() {
+            if let Some(frame_time) =
+                diagnostics.get(FrameTimeDiagnosticsPlugin::FRAME_TIME)
+            {
+                if let Some(frame_time) = frame_time.average() {
+                    let frame_time_ms = frame_time * 1000.0;
+                    text.value = format!("Frame Time: {:.0} ms", frame_time_ms);
+                }
             }
         }
     }
 }
+
+struct FrameTime;
