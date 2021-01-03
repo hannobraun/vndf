a06b0b2de2e8ecf04971a57f2ea79aa5dd3151bd
Hanno Braun <hb@hannobraun.de>
2021-01-03 09:09:19 +0100

    Convert free functions into associated functions

--- src/input.rs
+++ src/input.rs
@@ -19,65 +19,73 @@ pub struct InputPlugin;
 impl Plugin for InputPlugin {
     fn build(&self, app: &mut AppBuilder) {
         app.add_system(exit_on_esc_system.system())
-            .add_system(handle_mouse_click.system())
-            .add_system(handle_mouse_wheel.system());
+            .add_system(Self::handle_mouse_click.system())
+            .add_system(Self::handle_mouse_wheel.system());
+    }
+}
+
+impl InputPlugin {
+    fn handle_mouse_click(
+        mut state: Local<Option<MousePosition>>,
+        mut events: ResMut<Events<CursorMoved>>,
+        input: Res<Input<MouseButton>>,
+        windows: Res<Windows>,
+        bodies: Res<RigidBodySet>,
+        mut ships: Query<(
+            &mut Ship,
+            &camera::Focus,
+            &RigidBodyHandleComponent,
+        )>,
+        transforms: Query<&Transform>,
+    ) {
+        for event in events.drain() {
+            *state = Some(MousePosition {
+                position: event.position,
+                window_id: event.id,
+            });
+        }
+
+        if input.pressed(MouseButton::Left) {
+            if let Some(state) = state.deref() {
+                for (mut ship, focus, body) in ships.iter_mut() {
+                    let window = windows
+                        .get(state.window_id)
+                        .expect("Could not find window");
+                    let size = Vec2::new(
+                        window.width() as f32,
+                        window.height() as f32,
+                    ) / 2.0;
+
+                    let position = state.position - size;
+
+                    let camera = transforms.get(focus.camera()).unwrap();
+                    let position = camera.compute_matrix()
+                        * position.extend(0.0).extend(1.0);
+
+                    let body = bodies.get(body.handle()).unwrap();
+                    let direction = na::Vector2::new(position.x, position.y)
+                        - body.position().translation.vector;
+
+                    ship.direction_setting =
+                        Vec2::new(direction.x, direction.y);
+                }
+            }
+        }
+    }
+
+    fn handle_mouse_wheel(
+        mut events: ResMut<Events<MouseWheel>>,
+        mut players: Query<(&Player, &mut Ship)>,
+    ) {
+        for event in events.drain() {
+            for (_, mut ship) in players.iter_mut() {
+                ship.change_thrust_setting(event.y / 10.0);
+            }
+        }
     }
 }
 
 struct MousePosition {
     position: Vec2,
     window_id: WindowId,
-}
-
-fn handle_mouse_click(
-    mut state: Local<Option<MousePosition>>,
-    mut events: ResMut<Events<CursorMoved>>,
-    input: Res<Input<MouseButton>>,
-    windows: Res<Windows>,
-    bodies: Res<RigidBodySet>,
-    mut ships: Query<(&mut Ship, &camera::Focus, &RigidBodyHandleComponent)>,
-    transforms: Query<&Transform>,
-) {
-    for event in events.drain() {
-        *state = Some(MousePosition {
-            position: event.position,
-            window_id: event.id,
-        });
-    }
-
-    if input.pressed(MouseButton::Left) {
-        if let Some(state) = state.deref() {
-            for (mut ship, focus, body) in ships.iter_mut() {
-                let window = windows
-                    .get(state.window_id)
-                    .expect("Could not find window");
-                let size =
-                    Vec2::new(window.width() as f32, window.height() as f32)
-                        / 2.0;
-
-                let position = state.position - size;
-
-                let camera = transforms.get(focus.camera()).unwrap();
-                let position =
-                    camera.compute_matrix() * position.extend(0.0).extend(1.0);
-
-                let body = bodies.get(body.handle()).unwrap();
-                let direction = na::Vector2::new(position.x, position.y)
-                    - body.position().translation.vector;
-
-                ship.direction_setting = Vec2::new(direction.x, direction.y);
-            }
-        }
-    }
-}
-
-fn handle_mouse_wheel(
-    mut events: ResMut<Events<MouseWheel>>,
-    mut players: Query<(&Player, &mut Ship)>,
-) {
-    for event in events.drain() {
-        for (_, mut ship) in players.iter_mut() {
-            ship.change_thrust_setting(event.y / 10.0);
-        }
-    }
 }
