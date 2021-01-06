2255900c223462c42a5a21f9d6c9f9797ce10e98
Hanno Braun <hb@hannobraun.de>
2021-01-06 19:45:53 +0100

    Display number of rocks on screen

--- src/plugins/ui.rs
+++ src/plugins/ui.rs
@@ -1,19 +1,26 @@
 use bevy::{diagnostic::Diagnostics, prelude::*};
 
-use crate::ui::FrameTime;
+use crate::{
+    ui::{FrameTime, Rocks},
+    world::rock::Rock,
+};
 
 pub struct UiPlugin;
 
 impl Plugin for UiPlugin {
     fn build(&self, app: &mut AppBuilder) {
         app.add_startup_system(Self::setup.system())
-            .add_system(Self::update_frame_time.system());
+            .add_system(Self::update_frame_time.system())
+            .add_system(Self::update_rocks.system());
     }
 }
 
 impl UiPlugin {
     fn setup(commands: &mut Commands, asset_server: Res<AssetServer>) {
         commands.spawn(CameraUiBundle::default());
+
+        // TASK: Consolidate creation of text elements.
+
         commands
             .spawn(TextBundle {
                 text: Text {
@@ -37,6 +44,30 @@ impl UiPlugin {
                 ..Default::default()
             })
             .with(FrameTime);
+
+        commands
+            .spawn(TextBundle {
+                text: Text {
+                    font: asset_server.load("fonts/Tuffy_Bold.ttf"),
+                    style: TextStyle {
+                        font_size: 32.0,
+                        color: Color::WHITE,
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                },
+                style: Style {
+                    position_type: PositionType::Absolute,
+                    position: Rect {
+                        top: Val::Px(50.0),
+                        left: Val::Px(10.0),
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                },
+                ..Default::default()
+            })
+            .with(Rocks);
     }
 
     fn update_frame_time(
@@ -45,6 +76,15 @@ impl UiPlugin {
     ) {
         for mut text in elements.iter_mut() {
             FrameTime::format(&diagnostics, &mut text.value);
+        }
+    }
+
+    fn update_rocks(
+        mut elements: Query<&mut Text, With<Rocks>>,
+        rocks: Query<&Rock>,
+    ) {
+        for mut text in elements.iter_mut() {
+            Rocks::format(rocks.iter().count(), &mut text.value);
         }
     }
 }
--- src/ui.rs
+++ src/ui.rs
@@ -1,4 +1,4 @@
-use std::fmt::Write as _;
+use std::{fmt::Write as _, usize};
 
 use bevy::diagnostic::{Diagnostics, FrameTimeDiagnosticsPlugin};
 
@@ -15,5 +15,14 @@ impl FrameTime {
                     write!(s, "Frame Time: {:.0} ms", frame_time_s * 1000.0);
             }
         }
+    }
+}
+
+pub struct Rocks;
+
+impl Rocks {
+    pub fn format(num_rocks: usize, s: &mut String) {
+        s.clear();
+        let _ = write!(s, "Rocks: {}", num_rocks);
     }
 }
