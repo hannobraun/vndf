c911ff5eb06cdf18c76a0715a3fb1b50f63c9478
Hanno Braun <hb@hannobraun.de>
2021-01-10 11:48:42 +0100

    Convert free function to associated function

--- src/graphics/rock.rs
+++ src/graphics/rock.rs
@@ -6,28 +6,33 @@ pub struct RockPlugin;
 
 impl Plugin for RockPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.add_system(setup.system());
+        app.add_system(Self::setup.system());
+    }
+}
+
+impl RockPlugin {
+    fn setup(
+        commands: &mut Commands,
+        mut materials: ResMut<Assets<ColorMaterial>>,
+        rocks: Query<(Entity, &Rock), Without<RockGraphics>>,
+    ) {
+        for (entity, rock) in rocks.iter() {
+            commands
+                .insert(
+                    entity,
+                    SpriteBundle {
+                        material: materials
+                            .add(Color::rgb_linear(0.5, 0.5, 1.0).into()),
+                        sprite: Sprite::new(Vec2::new(
+                            rock.size(),
+                            rock.size(),
+                        )),
+                        ..Default::default()
+                    },
+                )
+                .insert_one(entity, RockGraphics);
+        }
     }
 }
 
 pub struct RockGraphics;
-
-fn setup(
-    commands: &mut Commands,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-    rocks: Query<(Entity, &Rock), Without<RockGraphics>>,
-) {
-    for (entity, rock) in rocks.iter() {
-        commands
-            .insert(
-                entity,
-                SpriteBundle {
-                    material: materials
-                        .add(Color::rgb_linear(0.5, 0.5, 1.0).into()),
-                    sprite: Sprite::new(Vec2::new(rock.size(), rock.size())),
-                    ..Default::default()
-                },
-            )
-            .insert_one(entity, RockGraphics);
-    }
-}
