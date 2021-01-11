20a5e6e4d2d479da39bb0a40f75a1e5510699c1c
Hanno Braun <hb@hannobraun.de>
2021-01-11 09:24:48 +0100

    Make constants public

--- src/graphics/mod.rs
+++ src/graphics/mod.rs
@@ -5,11 +5,11 @@ pub mod ship;
 
 use bevy::prelude::*;
 
-const COLOR_PLAYER: Color = Color::rgb_linear(0.0, 0.0, 1.0);
+pub const COLOR_PLAYER: Color = Color::rgb_linear(0.0, 0.0, 1.0);
 
-const LAYER_WORLD: f32 = 0.0;
-const LAYER_MARKER: f32 = 0.5;
-const LAYER_UI: f32 = 1.0;
+pub const LAYER_WORLD: f32 = 0.0;
+pub const LAYER_MARKER: f32 = 0.5;
+pub const LAYER_UI: f32 = 1.0;
 
 pub struct GraphicsPlugin;
 
