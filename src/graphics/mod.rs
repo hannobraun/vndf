b4311f3ee60529866cf6964e7f82ce0bc89849ae
Hanno Braun <hb@hannobraun.de>
2021-01-18 18:33:26 +0100

    Add task

--- src/graphics/mod.rs
+++ src/graphics/mod.rs
@@ -10,3 +10,5 @@ pub const COLOR_PLAYER: Color = Color::rgb_linear(0.0, 0.0, 1.0);
 pub const LAYER_WORLD: f32 = 0.0;
 pub const LAYER_MARKER: f32 = 0.5;
 pub const LAYER_UI: f32 = 1.0;
+
+// TASK: Display target, if selected.
