caa0fa85724d78328c1459870aff5010fa964900
Hanno Braun <hb@hannobraun.de>
2021-01-20 19:38:11 +0100

    Track whether target position has changed

--- src/world/target.rs
+++ src/world/target.rs
@@ -3,14 +3,25 @@ use bevy::prelude::*;
 #[derive(Debug)]
 pub struct Target {
     position: Option<Vec2>,
+
+    /// Tracks whether `position` has changed
+    ///
+    /// It would be nicer to use Bevy's built-in change tracking for this, but
+    /// that depends on the order of system execution. Issue:
+    /// https://github.com/bevyengine/bevy/issues/68
+    changed: bool,
 }
 
 impl Target {
     pub fn new() -> Self {
-        Self { position: None }
+        Self {
+            position: None,
+            changed: false,
+        }
     }
 
     pub fn set(&mut self, position: Vec2) {
-        self.position = Some(position)
+        self.position = Some(position);
+        self.changed = true;
     }
 }
