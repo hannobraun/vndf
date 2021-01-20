e013bcc1c028f0177da681d4116ae6735cd893cc
Hanno Braun <hb@hannobraun.de>
2021-01-20 19:43:34 +0100

    Rename struct field

--- src/world/target.rs
+++ src/world/target.rs
@@ -9,28 +9,28 @@ pub struct Target {
     /// It would be nicer to use Bevy's built-in change tracking for this, but
     /// that depends on the order of system execution. Issue:
     /// https://github.com/bevyengine/bevy/issues/68
-    changed: bool,
+    has_changed: bool,
 }
 
 impl Target {
     pub fn new() -> Self {
         Self {
             position: None,
-            changed: false,
+            has_changed: false,
         }
     }
 
     pub fn set(&mut self, position: Vec2) {
         self.position = Some(position);
-        self.changed = true;
+        self.has_changed = true;
     }
 
     pub fn has_changed(&self) -> bool {
-        self.changed
+        self.has_changed
     }
 
     pub fn position(&mut self) -> Option<Vec2> {
-        self.changed = false;
+        self.has_changed = false;
         self.position
     }
 }
