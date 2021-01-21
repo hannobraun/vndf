c13e3009b20744bf1af1bf957c12eb93e200a859
Hanno Braun <hb@hannobraun.de>
2021-01-21 20:59:53 +0100

    Rename system

--- src/plugins/graphics/target.rs
+++ src/plugins/graphics/target.rs
@@ -6,12 +6,12 @@ pub struct TargetPlugin;
 
 impl Plugin for TargetPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.add_system(Self::add_components.system());
+        app.add_system(Self::update_graphics.system());
     }
 }
 
 impl TargetPlugin {
-    fn add_components(mut targets: Query<&mut Target>) {
+    fn update_graphics(mut targets: Query<&mut Target>) {
         for mut target in targets.iter_mut() {
             if target.has_changed() {
                 // TASK: Display target, if selected.
