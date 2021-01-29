3abe074914fa578675b5e1db8fd4d878f4e7508a
Hanno Braun <hb@hannobraun.de>
2021-01-29 19:26:21 +0100

    Remove initial projectile

--- src/plugins/world/projectile.rs
+++ src/plugins/world/projectile.rs
@@ -1,18 +1,7 @@
 use bevy::prelude::*;
-use bevy_rapier2d::na::Vector2;
-
-use crate::world::projectile::Projectile;
 
 pub struct ProjectilePlugin;
 
 impl Plugin for ProjectilePlugin {
-    fn build(&self, app: &mut AppBuilder) {
-        app.add_startup_system(Self::setup.system());
-    }
-}
-
-impl ProjectilePlugin {
-    fn setup(commands: &mut Commands) {
-        commands.spawn(Projectile::create(Vector2::new(100.0, 0.0)));
-    }
+    fn build(&self, _app: &mut AppBuilder) {}
 }
