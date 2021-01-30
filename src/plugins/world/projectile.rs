14bdfd90cc77c8314326027f0f9239c0cabce467
Hanno Braun <hb@hannobraun.de>
2021-01-30 12:18:49 +0100

    Add tasks

--- src/plugins/world/projectile.rs
+++ src/plugins/world/projectile.rs
@@ -5,3 +5,6 @@ pub struct ProjectilePlugin;
 impl Plugin for ProjectilePlugin {
     fn build(&self, _app: &mut AppBuilder) {}
 }
+
+// TASK: Destroy projectiles on impact.
+// TASK: Apply force to bodies hit by projectile on impact.
