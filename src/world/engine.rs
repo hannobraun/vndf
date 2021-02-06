6d823d42c1988d94f7a7424cdf8b6be6f26158ea
Hanno Braun <hb@hannobraun.de>
2021-02-06 13:05:50 +0100

    Rename variable

--- src/world/engine.rs
+++ src/world/engine.rs
@@ -1,32 +1,32 @@
 use bevy_rapier2d::{na::Vector2, rapier::dynamics::RigidBody};
 
 pub struct Engine {
-    thrust_setting: f32,
+    thrust: f32,
     max_thrust: f32,
 }
 
 impl Engine {
     pub fn new() -> Self {
         Self {
-            thrust_setting: 0.0,
+            thrust: 0.0,
             max_thrust: 1_000_000.0,
         }
     }
 
     pub fn thrust_setting(&self) -> f32 {
-        self.thrust_setting
+        self.thrust
     }
 
     /// Change the thrust setting by the given amount
     pub fn change_thrust_setting(&mut self, change: f32) {
-        self.thrust_setting += change;
-        self.thrust_setting = f32::min(f32::max(self.thrust_setting, 0.0), 1.0);
+        self.thrust += change;
+        self.thrust = f32::min(f32::max(self.thrust, 0.0), 1.0);
     }
 
     pub fn apply_thrust(&self, ship: &mut RigidBody) {
         let direction = ship.position().rotation * Vector2::new(1.0, 0.0);
 
-        let thrust = self.max_thrust * self.thrust_setting * direction;
+        let thrust = self.max_thrust * self.thrust * direction;
         ship.apply_force(thrust, true);
     }
 }
