0c52a7e282f13c370cc300744b5bbc6c6fa79d75
Hanno Braun <hb@hannobraun.de>
2021-02-09 19:35:46 +0100

    Prevent projectiles from colliding with each other

--- src/world/projectile.rs
+++ src/world/projectile.rs
@@ -1,7 +1,10 @@
 use bevy::prelude::*;
 use bevy_rapier2d::{
     na::{Point2, Vector2},
-    rapier::{dynamics::RigidBodyBuilder, geometry::ColliderBuilder},
+    rapier::{
+        dynamics::RigidBodyBuilder,
+        geometry::{ColliderBuilder, InteractionGroups},
+    },
 };
 
 pub const SIZE: Vec2 = Vec2 { x: 10.0, y: 10.0 };
@@ -18,7 +21,10 @@ impl Projectile {
             RigidBodyBuilder::new_dynamic()
                 .translation(position.x, position.y)
                 .linvel(velocity.x, velocity.y),
-            ColliderBuilder::cuboid(SIZE.x / 2.0, SIZE.y / 2.0).sensor(true),
+            ColliderBuilder::cuboid(SIZE.x / 2.0, SIZE.y / 2.0)
+                .sensor(true)
+                // Projectiles collide with everything except other projectiles.
+                .collision_groups(InteractionGroups::new(0x1, !0x1)),
         )
     }
 }
